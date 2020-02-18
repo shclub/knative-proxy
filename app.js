14c7bef47919adedb9980965ea26d1050d6af112
shclub <41466732+shclub@users.noreply.github.com>
2020-02-18 18:10:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,596 @@
+Last login: Mon Feb 17 17:42:24 on ttys007
+You have mail.
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+jake-MacBook-Pro:~ shclub$ ls
+Applications			ca.key				iCloud Drive (Archive) - 9	registry.tar
+Calibre Library			company_backup			index.html			repos
+Desktop				config.yml			install.py			response.txt
+Documents			default-soapui-workspace.xml	invest_search			response2.txt
+Downloads			docker-registry-ui		invest_search_20190415		response3.txt
+Dropbox				epc-test2			invest_search_backup.tar.gz	sample_parser.py
+Google Drive			express-cors-demo		invest_search_compose		sample_parser2.py
+HTTP Header Manager.jmx		fabric8-installer		investing-1.py			savefile_invest.py
+Hygieia				fastoredis.log			investing_title.py		savefile_invest_old.py
+IMG_3402.JPG			fraud_detection			investing_title_bs.py		savefile_invest_old2.py
+IdeaProjects			fungus.txt			istio-0.8.0			screenshot.png
+Library				ghostdriver.log			istio-1.4.4			screenshot12.png
+Movies				gofabric8-darwin-amd64		jake.tar			send_telegram.py
+Music				goobye-display.yaml		jdeveloper			serverless
+NFDS-Crawler			googbye-trigger.yaml		jmeter.log			sidecar_backup
+NFDS_test.tar			hello-display.yaml		kafka-producer.py		soapui-settings.xml
+OneDrive			hello-trigger.yaml		latest.txt			springboot-sample
+Oracle				hygieia-core			log.txt				src
+Pictures			hygieia-docker-compose		mouse.py			telegram_bot_test.py
+Postman				hygieia-scm-github-collector	node_modules			temp
+Public				hygieia-starter-kit		package-lock.json		tempLoadUI
+README.md			iCloud Drive (Archive)		package.json			test.oy
+TEST-workspace.xml		iCloud Drive (Archive) - 1	pgadmin.log			test.py
+Test Plan.png			iCloud Drive (Archive) - 2	phantom_daum.py			test_docker_comp
+Thread Group.jmx		iCloud Drive (Archive) - 3	phantom_daum_simple.py		uninstall.sh
+UOS_LIKELION_python		iCloud Drive (Archive) - 4	pockermon.py			vagrant_jake
+VirtualBox VMs			iCloud Drive (Archive) - 5	ppomppu_monitor			values.yaml
+api				iCloud Drive (Archive) - 6	ppomppu_monitoring_backup	vmware
+bea				iCloud Drive (Archive) - 7	producer.yaml			webpack.config.js
+ca.crt				iCloud Drive (Archive) - 8	python_ui			微云同步盘
+jake-MacBook-Pro:~ shclub$ cd Downloads
+jake-MacBook-Pro:Downloads shclub$ ls
+$RECYCLE.BIN
+%EC%9D%B4%EC%9C%A4%EC%8B%A0_20191021_44.pdf
+0.Tuning_Guide(B2C)_bzwz7wqvbvg3g.AIA.env_12-sep-2012.doc
+00.SQL tuning 진행관리-AIT-AIA_120906.xlsx
+0830(안면도).zip
+1
+1.8.5.1B_201513211614.zip
+1.xml
+10
+100GOPRO
+11
+111
+112
+113
+114
+115
+116
+117
+118
+119
+11시43분42초_cancel
+11시45분18초_Cancel
+12
+12-96_6k47.pdf
+120
+121
+122
+12시간만에영어귀가들린다!!매직리스닝!!!!
+13236H00305429G_ABM
+13259F10364819G_ICIS_UPDATE_COMPLETE.xml
+13259F10364819G_fulfill.xml
+13292E20016989G_ABM
+13292E20016989G_EBM
+13303G40495039G_ABM
+13303G40495039G_SalesOrderEBM
+13311A20034289G.xml
+1_13232F30293629G
+1_suspend
+1화_가상화란 무엇인가(3).pdf
+2
+2012-8 수시채용 전체.ofl
+2013-08-30_kt_Genesis_R1.1_관통테스트_진행계획협의_회의록.doc
+2013-10-01 가입제한 실주민번호 연동 오류.xlsx
+20130617154950_18ea6d233eb7aa5d434040e82d685652.jpg
+20141027155921_9b7sqK8F_facebook_1414338637472.jpg
+2014년_SM개발예산_정리_1017_v1_FM_과금_빌링 추가.xlsx
+2014년_SM개발예산_정리_AIA.xlsx
+2019-Winter-HGU-Machine-Learing-Camp-master.zip
+20190710_KT_Splunk_소개.pptx
+20190710_KT_스플렁크소개
+2_ 13232F30293629G
+2_13232F30293629G
+2_Install Jenkins on ICP.html
+2_suspend
+3
+3차 기성 증빙 scan.pdf
+3차기성CR그룹별투입MM(총괄)_131002_제출.xlsx
+3차기성CR그룹별투입MM(총괄)_131002_제출_kt검토.xlsx
+4
+5
+6
+7
+7zX_1.7.1.dmg
+7박 8일간 씨엠립에서 방문한 식당 후기 _ 네이버 카페.pdf
+8
+9
+94d7eb48c7bff4681697fdeb4d3196e9.json
+9781849695107-PENETRATION_TESTING_WITH_THE_BASH_SHELL.pdf
+AAA
+AABC_Async_ServiceReactive_AQ_Configuration.doc
+ABM_13220G50516889G_NewCombined.xml
+AIA XRef Search Service v_0.1.pptx
+AIAErrorHandlingPlan.xlsx
+AIAErrorHandlingPlan_decrypt.xlsx
+AIA_ChangeRequestForm_v0.1.doc
+AIA_CustomDomain_DivisionMethod_v0 5.pptm
+AIA_DEV1_QUEUE.txt
+AIA_Purge.pptm
+AIA_Purge_v3.pptm
+AIA_Resequencer_Patch_20130423_v0.1.pptx
+AIA매핑확인요청용_BC목록_20130717.xlsx
+AMM1
+AMM2
+AMM_Changes_20130704.pptx
+AMO_AIA_Daily_Monitoring_Report_CRM_OSM_Issues_12Nov.xlsx
+AMO_resource_assessment_역량수준조사.xlsx
+ASUS TM-AC1900
+AWS_SA_Associate.pdf
+AWS_SA_Associate_answer_1.0v.pdf
+All order lines are completed.xml
+All_Item_Completed_Internet_Suspend.xml
+Archive and Purge Requirement_(BRM,Siebel이외)_2013 05 14.xlsx
+Archive.zip
+B2C_고객_모델링_V0.6_20130629.pptx
+BPEL_ERD_SPEC.zip
+BRM JCA Error Code 정리.xlsx
+BRMAdapterServletClient.war
+BSS-ERP_Interface연동가이드_20130218.pptx
+BSSINT_AIA_Operation_Manual-R0_9_FNL_v0.9.docx
+BSSINT_AIA_운영전환방안_FNL_v0.9.pptx
+BSSINT_LEG_TD_INF-OM-0162_IO Mapping(ND1)v0.1.xlsx
+BSSOSS-98496_Date.xlsx
+Barnes & Noble1.pdf
+Barnes & Noble2.pdf
+Barnes & Noble_RECEIPT_1.pdf
+Barnes & Noble_receipt2.pdf
+Biz3.pdf
+BlackBerry Link.dmg
+Bulk_Inbound Order.pptx
+CM_and_Shakeout_Process_Presentation.ppt
+CSG for CROSSLOB - Example.xls
+Cache서버.pdf
+ChromeStandaloneSetup64.exe
+Cloud_DevOps_KT.pptx
+CustomFulfillmentOrderEBO.xsd
+CustomProvisioningOrderEBO.xsd
+DOMA.png
+Debut
+Docker.dmg
+E2E 대상 List_R1 0_0819.xls
+E2E_CSG_HighLevelSolution_v1.0.pptx
+E2E_Mapping_SyncCustomer_CRM2AIA2CIA_취합_CRM_Reviewed.xlsx
+E2E_Session_-_Charge_Sharing_V0.2.pptx
+EBM.xml
+EBM2.xml
+EBM3.xml
+EBM_13220G50516889G_NewCombined.xml
+Edit29
+Edit30
+FW_RT_AC68U_30043763626 (2)
+FW_RT_AC68U_300438445717.zip
+FileZilla_3.46.3_macosx-x86_sponsored-setup.dmg
+Following the Agenda.docx
+G2L SyncCustomer성능20131111.pptx
+G2L 성능 및 오류 현황 20131106.pptx
+GARAGEK Slack export Jan 17 2019 - Jan 18 2020.zip
+GoogleDriveBackup
+Graph DB 개념과 활용 전략.pdf
+HP서버-13년07월.xlsx
+Helm Basics and Creating Chart.pdf
+HotBill and Inquire Penalty_Flow_V0.1.pptx
+IBM Garage
+IBM Garage for KT (20190118).zip
+IBM Garage for KT_외부반출
+IBM_Garage
+IBM_Garge_외부공유버전
+ICP MVP Environment.xlsx
+ICP Test Environment_KT (1).xlsx
+ICP Test Environment_KT.xlsx
+IDG_Whitepaper_ServerlessWhitepaper.pdf
+IKC_STUCK
+IMG_0482.jpeg
+IMG_3540.jpg
+IMG_3597.png
+IMG_4781.JPG
+IMG_4782.JPG
+IMG_4783.JPG
+IMG_6173.jpeg
+IMG_6180.jpeg
+IMG_6187.JPG
+IMG_9443.JPG
+IPTV_INTERNET_통합_V0.1_1차우선적용대상식별_0725_AIA_0729.xlsm
+Incident report 10 Dec 2013 All Par_working by shim.xls
+Instana-201906.pptx
+Intermittent Failure in executing BRM Opcode [Compatibility Mode].pdf
+Interview Schedule_0802_kt assigned v0.1.xlsx
+JIRA 4332.docx
+JIRAs and Status.ods
+JIRAs and Status.pdf
+Jake-NAS_20180208.dss
+KBANK인터뷰
+KT Customer_Sync Trigger.zip
+KT-Demo MVP v1.1.pptx
+KTPIP JMS Configuration list.xlsx
+KT_B2B Platform_Final_201907(salesforce).pdf
+KT_BSS_Development_Env_User_Access_Request(DEV0) (1).xlsx
+KT_BSS_Development_Env_User_Access_Request(DEV0).xlsx
+KT_BSS_Development_Env_User_Access_Request(DEV0)_siebel.xls
+KT_Genesis_ND2Cycle1_TechTesting_InterimReport_20130627_v3_draft..pptm
+KT_NSCR_Custom_Trigger_27May2013.docx
+KakaoTalk_Chat_My Love_2017-01-31-21-15-10.csv
+KakaoTalk_Image_2019-05-16-11-18-54.jpeg
+KakaoTalk_Image_2019-05-16-11-18-58.jpeg
+KakaoTalk_Image_2019-05-16-11-19-12.jpeg
+KakaoTalk_Image_2019-05-16-11-19-30.jpeg
+KakaoTalk_Image_2019-05-16-11-26-23.jpeg
+KakaoTalk_Image_2019-08-14-18-50-02.jpeg
+KakaoTalk_Photo_2019-08-14-18-50-08.jpeg
+KakaoTalk_Photo_2019-08-14-18-50-33.jpeg
+KakaoTalk_Photo_2019-08-14-18-50-40.jpeg
+KakaoTalk_Photo_2019-08-14-18-50-48.jpeg
+KakaoTalk_Photo_2019-08-14-18-50-54.jpeg
+KakaoTalk_Photo_2019-08-14-18-51-00.jpeg
+KakaoTalk_Photo_2019-08-14-18-51-06.jpeg
+KeepingYouAwake-1050001.zip
+KeepingYouAwake.app
+MSA개발설명회
+MacHg1.0.0Beta.zip
+MacHgLatest.zip
+MailbirdSetup.exe
+Microsoft Windows Server 2012 x64-iNDiSO [h33t].torrent
+MiddleZone fallout_20130613_COM_Updated.pptx
+MiddleZone_오더재처리내역_20130720.pptx
+MnetMusic_beta4.zip
+Mnp_Order_Product_Payment_0508.ppt
+Monitoring_sql.txt
+Mounty.dmg
+Multi Schema _20130704.pptx
+ND1+SIT+CROSS+SIR+(KT).xls
+NDrive
+NEW_GOMPLAYERSETUP.EXE
+NRDS_ABM.xml
+NetworkConnect.pkg
+Next_109.msi
+NoSQL & MSA(3h).pptx
+O41100113mod.zip
+OBG밴드_프로젝트_신부_합창 - 2017. 6. 14. 오전 3.51.m4a
+OSBvipDownErrorgroupIds_23Sep11PM.xls
+OSM_COM_자료
+OSS_R1.1_[AIA].C101.AIA_Interface_Specification_O2C_FO_SOMF2ICIS_WIFI_Drop2_V1.0.xlsx
+OpenShift_Container_Platform-4.1-Serverless_applications-en-US.pdf
+OrderMilestoneSequence.docm
+Orderfallout Error Code 정리_MDZ_SRTT_0626_ 통합_2013.07.08.xlsx
+POEBM_13317H30086759G
+PRD_DB_Access_ReadOnly.xlsx
+Package Discount Issue_3.pptx
+PblCJK_K10.pbl
+PharosTrace_UserGuide_kr_v40.pdf
+Pharos_20140108
+Postman-osx-7.18.0.zip
+Product Issues v0.2.ppt
+Profile_0802.zip
+Prov_Internet_Modify(adding_VAS_TimeCodi).xml
+Provisioning EBM_NewInstall.xml
+ProvisioningOrder_Combined_IDMS.xml
+PulseSecureAppLauncher.dmg
+Python_vscode
+R0.9x_AIA_Issues_ContingencyPlan_v5.pptm
+README.htm
+RFP_Cache_Server_평가항목_Feedback답변.xlsx
+RODOD Timezone format v0.3.ppt
+RPi-sd card builder v1.2.app
+RPi-sd card builder v1.2.zip
+RT-AC68U_384.10_0
+RT-AC68U_384.10_0.zip
+RT-AC68U_384.12_0
+RT-AC68U_384.12_0.zip
+Rescue_2000
+SC 보고 준비_1(수정).xlsx
+SDCardFormatterv5_Mac.zip
+SDFormatterv4.zip
+SIT1-IPTV_준공수신-soapui-project.xml
+SIT1-IPTV_준공수신-sql.txt
+SIT1-Internet-준공수신-soapui-project.xml
+SIT1-Internet-준공수신-sql.txt
+SOA Infra.jpeg
+SOA Suite 11g in VM Ware.pdf
+SP6+SIT+Cross+SIR+(KT).xls
+SP64420.zip
+SWUpdate_2.2.1.5.ZIP
+Schedule for 3rd Week.docx
+SearchResults.html
+Ship_or_Sheep_CD1.rar
+Ship_or_Sheep_CD2.rar
+Ship_or_Sheep_CD3.rar
+Sourcetree_3.0.1_205.zip
+Splunk @ Telco for APAC Use Case EXTERNAL
+Splunk Company and Platform.pptx
+Sprint 7 E2E Session - Special Rating VAS v0.2.pptx
+Step by Step Installation of Hygieia Latest version in centos 7.6.pdf
+SumApp-master-0b6ef0ff145fe0a59a738e0d919d38835834f92e.zip
+TCS.AIA.PrimarySupport.RolesAndResponsibilities.xlsx
+The Azure Kubernetes Workshop.pdf
+Thumbs.db
+Tmax브로슈어.pdf
+TransInfo_HLS
+Typora.dmg
+UPDATE PRM AFTER COMPLETE.xml
+USA_BusinessTrip
+UltraEdit_20.00.0.12.dmg
+Unit_Test_Scenario_SR2_ExternalGUIRole_0.4.xlsx
+Untitled 3.mp4
+UpdateSalesOrderEBM
+VNC-Viewer-6.17.1113-MacOSX-x86_64.dmg
+WIN7_LITE_IE8_86.txt
+WeChatMac.dmg
+Xref_DataMigration.docx
+[LINE]주깔 백두산 프리미엄.txt
+[klasses]XiaoYi Camera_1.5.0.apk
+[klasses]Yi+Camera_1.2.12.apk
+acv405.zip
+aia-11g-performance-tuning-1915233.pdf
+aia2xperformancetuningshowcase-1610683.pdf
+astx.dmg
+auto_click.exe
+awsomedaymay2019krmodule1-190521062845.pdf
+bluecompute-ce-0.0.9.tgz
+bpel-hello-world-128219.pdf
+capture.PNG
+chromedriver
+chromedriver_linux64.zip
+client1.ovpn
+com.github.dawndiy.bifrostv.apk
+company_backup
+config
+config.json
+data1.txt
+davinci_day
+day2track4session2-190419040455.pdf
+day2track4session2-190419040455.pptx
+day2track4session3-190419040457.pdf
+day2track4session3-190419040457.pptx
+day2track5session3-190419041454.pdf
+day2track5session3-190419041454.pptx
+desktop.ini
+dev1_connection.jpg
+developerjrtdd-190419063907.pdf
+devops-master.zip
+docker-compose-usages-public-170224174113.pdf
+ee46c4c6be1df717b3b82f55b531912f.json
+error.txt
+example.yaml
+executive-summary-a-more-secure-everywhere.pdf
+fn-ppomppu-2.yaml
+fonts_nanum.gothic.2013
+git_test
+helm-rbac.yaml
+home
+iTunes
+icp-on-rhel
+icp-on-rhel.zip
+ide-160403040004.pdf
+index.html
+install-unibox.zip
+install_flash_player_osx_ppapi.dmg
+installcentos-master.zip
+intern
+intern_33
+intern_backup
+iphone1 (1).zip
+isky-dvd-creator-intel_full322.dmg
+jake_intern
+jjang.zip
+k8s-edu-master
+k8s-edu-master.zip
+k8s_meetup_20181123.zip
+kafka-confluent-platform
+kafka-exporter-overview_rev5.json
+kafka-topics_rev1.json
+kafka_rev1 (1).json
+kafka_rev1 (2).json
+kafka_rev1.json
+knative-proxy
+knative-spring-dbcrud-master.tar
+knative-spring-oauth2-master.tar
+kos-mvp-master.t__
+kos-mvp-master.tar.gz
+kos-mvp-master.zip
+kt BIT Program - PMO - 개발지원 포탈 가이드 v1 1_kor.docx
+kt Genesis - R1.0 - TA423 AIA Foundation Pack 11.1.1.5 Cluster Installation and Configuration Guide v3.9.docx
+kt Genesis_OSSR1.1_OSM-SOMM-E_Low Level Design_v0.5.doc
+kt bit BSSOSS_R1.0_AIA Patch Baseline for R0.9 & R1.0_20130702.pptx
+ktBIT_2차오픈_리소스예상_취합용_v1.62_AIA.xlsx
+kt_Genesis_-_R1_0_-_Interface_Inventory_v3_01_AIA.xlsx
+kt_Genesis_AIA_XREF_Purge.docx
+kt_Genesis_Interface_Scope_VBSS01_ND2_Interface_v0.1.0_working.xlsx
+kt_Genesis_Interim_AIA_WBS.mpp
+kt_Genesis_MPMO_BIT_Review_log_(AIA_Installation_and_Configuration_Guide_v.3.9)_v1.0.xlsx
+kt_Genesis_MPMO_Deliverables_Tracker_AIA_v2.22_최종.xlsx
+kt_Genesis_R0.9x_SPT_Summary_v0.9_20131121.pptm
+ktds Capa내재화 지표_중간조사_v1.0_10월_Integration_AIA.xlsx
+ktds Capa내제화 핵심인력_v1.3_전체_20130506_공유.xlsx
+kubernetes-201811-meetup-master.zip
+kubernetes-apps_rev1.json
+kubernetes-cluster-health_rev1.json
+kubernetes-cluster-monitoring-via-prometheus_rev1.json
+kubernetes-cluster-monitoring_rev2.json
+kubernetes-cluster-prometheus_rev1.json
+kubernetes-helm-180407011508 (1).pdf
+kubernetes-helm-180407011508.pdf
+kubernetes-node-exporter-full_rev1.json
+launch (1).ica
+launch (2).ica
+launch (3).ica
+launch (4).ica
+launch (5).ica
+launch (6).ica
+launch (8).ica
+launch.ica
+medis-v0.5.0-mac-x64.zip
+mentoring
+messageImage_1517646218227.jpg
+milestone update EBM sequence Number.png
+mixer-dashboard_rev1.json
+monitoring-tracing-zipkin.yaml
+ms_azure-workshop
+my-project
+my-project2
+mypage-mvp-master.zip
+naihil-ppomppu1.html
+new  6.yml
+node-exporter_rev5.json
+nuclio
+nuclio_backup
+openfaas-cron-connector-node10ex-dbselect-master.tar
+openfaas-go-hello-master.tar
+openfaas-go-kafkamessage-master.tar
+openfaas-java8-hello-master.tar
+openfaas-node-host-os-history-master.tar
+openfaas-node-rest-get-src-master.tar
+openfaas-node-rest-put-src-master.tar
+openfaas-node-send-email-src-master.tar
+openfaas-node10ex-cronlog-master.tar
+openfaas-node10ex-dbselect-master.tar
+openfaas-node10ex-kafkaapi-master.tar
+openfaas-node10ex-kafkatodb-master.tar
+openfaas-node10ex-openapi-master.tar
+openfaas-nodejs-select-master.tar
+openfaas-py-fileproducer-master.tar
+openfaas-py-kafkatodb-master.tar
+openfaas-py-kafkatodb-master.tar.bz2
+openfaas-py-kafkatodb-master.tar.gz
+openfaas-py-loadimages-master.tar
+openfaas-py-processimages-master.tar
+openfaas-py3-dbselect-master.tar
+openfaas-template-master.tar
+oracle
+order level amending status management issue.pptx
+osmc-installer.exe
+package-lock.json
+pcfpks-springone-recap-2017-v1hshin-180112080339.pdf
+phantomjs-2.1.1-linux-x86_64.tar.bz2
+phantomjs-2.1.1-macosx
+phantomjs-2.1.1-macosx 2
+pharosExcel_strange.xls
+pharosExcel_syncCustomer_20140108.xls
+pharos_strange
+ppomppu
+project_pytesseract.py
+projecttest
+putty.exe
+quickstartguidesoasuite11gr1ps3-313245.pdf
+raspbmc-win32.zip
+rdpwrap.zip
+robo3t-1.2.1-darwin-x86_64-3e50a65.dmg
+robo3t-1.3.1-darwin-x86_64-7419c40.dmg
+rt-ac68u_1.0.2.0_us
+selfinstall.xml_13150L20393709G.xml
+selfinstall.xml_13150L20393709G_EBM.xml
+serverlesslondon2019-faascompositionusingkafkaandcloud-events-191107100904.pdf
+shellinabox-2.20-5.el7.x86_64.rpm
+site.zip
+siw-workshop-labs
+siw-workshop-labs.zip
+snp_screen_shot.png
+splunk
+src
+strimzi-kafka-exporter_rev1.json
+switchmaci.zip
+syncSharingBRM-call-soapui-project.xml
+talkv_high.mp4
+tar_gather
+tsetup.0.5.21.trx
+uiso93pes.zip
+updateMilestone
+updateSalesOrderEBM
+utf-8''TIBCO EMS 솔루션을 활용한 MQ 대체 방안.pptx
+v2ray-windows-64.zip
+values (1).yaml
+values (2).yml
+values.yaml
+vuejs-boilerplate
+webpack-simple
+webpack-simple2
+ws-bpel.pdf
+wsbpel-v2.0-OS.pdf
+xref table.xlsx
+yoonshin
+~$2013-10-01 가입제한 실주민번호 연동 오류.xlsx
+~$AMO_AIA_Daily_Monitoring_Report_CRM_OSM_Issues_12Nov.xlsx
+~$AMO_resource_assessment_역량수준조사.xlsx
+~$Biz 부서 kickoff Communication deck_v1.0..pptx
+~$R0.9x_AIA_Issues_ContingencyPlan_v5.pptm
+~$Sprint 7 E2E Session - Special Rating VAS v0.2.pptx
+~$kt_Genesis_MPMO_Deliverables_Tracker_AIA_v2.22_최종.xlsx
+~$rint 3 CRM HLD v045..docx
+가족.png
+가족관계.jpg
+가족어릴때통합.jpeg
+고객 동기화 오류 처리 이력_20131006_V1.3.xlsx
+고객정보 동기화 PPD 테스트 결과.pptx
+고객정보 동기화 인터페이스 효율화 방안_V5.0.pptx
+고객정보 동기화 인터페이스 효율화 방안_V6.0.pptx
+고객정보_동기화_인터페이스_효율_V7.0.pptx
+금혼식_투샷.jpeg
+김건모 전집
+나의드라이버스윙.mp4
+내여권.jpeg
+단체_결혼사진.jpeg
+매입자료(예탁원제출자료)1.tif
+매입자료(예탁원제출자료)2.tif
+맥_메신저.pdf
+명함
+목록.xlsx
+무선 랜 드라이버_15.0.0.75_64bit.zip
+물놀이 Playing in the water (걸어서 세계속으로 오프닝송 Opening Music of Go to the World on Foot).mp3
+민정
+방화벽요청서_BSS_Service_Expose_20130206.xls
+배아생성동의서.hwp
+사랑.png
+사업혁신인프라담당_주간보고_20131104.pdf
+샤오미_cctv_backup
+석규네사진_201410
+소니유럽백업
+수시채용사원 입문교육 (8기) 연락처.xlsx
+신부입장.jpeg
+아스날시연동영상20190923_v2.mp4
+암닥스1.m4a
+암닥스2.m4a
+압사라 공연과 디너 뷔페 추천 부탁드립니다 _ 네이버 카페.pdf
+엑셀.jpg
+여행.png
+// the question (req.header('Origin')).
+예전_부모님_튜샷.jpeg
+옛날을더듬어.png
+오브젠
+올라_윤신.jpeg
+올라_윤신_2.jpeg
+우리동영상.mp4
+원판.zip
+윈도우7_정품인증_A.I_1003H
+윤신_런던_합침.jpeg
+윤신여권.jpeg
+융창조감도.jpeg
+이모들과.jpeg
+이석환 이윤신
+이석환()0020040201002162301305 (1).pfx
+이석환()0020040201002162301305.pfx
+
+이석환(730120)-2013년자료.pdf
+이석환(730120)-2018년도자료.pdf
+이석환♡이윤신
+이티켓(이석환).pdf
+일상.png
+자발적 자기완결형 개발 문화를 위한 GSShop 고군분투기.pdf
+장동건+주진모 문자 내용.pdf
+종교.png
+준공수신EBM_Complete.xml
+친척 주소록 2017.hwp
+친척주소록 copy.x__.xlsx
+친척주소록.pdf
+친척주소록.xlsx
+케잌.jpeg
+팔순동영상
 // app.js - A proxy for a Knative service.
 // Necessary because XHRs can't call Knative services as far as I can tell.
 // Written by Doug Tidwell, dtidwell@redhat.com
@@ -36,42 +629,4 @@ reassureTheClient = function(req, res) {
 // This implements the POST verb.
 pub_message = function(req, res){
     console.log(req.body);
-    //console.log(`We\'re in the proxy: ${req.body.json()}`);
-
-    // We obviously need to know the URL of the service as well as
-    // the value for the HOST header. If there's an environment
-    // variable for these values, that's what we use; otherwise
-    // we've got hardcoded values instead.
-    const url = 'http://211.252.86.33:31380/iphone';
-    const headerValue = 'pub-python.jake.211.252.86.33.xip.io';
-
-    nodeFetch(url, {
-        timeout: 90000,
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'topic' : 'test1',
-            'host_kafka' : '211.252.86.33:31009',
-            'Host': headerValue
-        },
-        body: JSON.stringify(req.body)
-    })
-//    console.log(`Knative body ${body}...`);
-    // Handle the requests via promises here
-    .then(response => response.json())
-    .then((responseJson) => {
-        res.set({'Access-Control-Allow-Origin': req.header('Origin'),
-                 'Cache-Control': 'no-cache, no-store, must-revalidate'})
-            .json(responseJson)
-            .status(200);
-    });
-};
-
-// Define the methods for the POST and OPTIONS verbs
-app.route('/iphone')
-    .post(pub_message)
-    .options(reassureTheClient);
-
-app.listen(port);
-console.log(`Knative proxy service running on port ${port}...`);
+"app.js" 78L, 2911C
